781428a77305527b350b1752b6dd95160b4df69c
Exitae <exitae337@gmail.com>
2026-04-04 13:23:59 +0300

    All logic - feature config

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
+import "github.com/exitae337/walletgorest/internal/config"
+
 func main() {
 	// 1. Read and Init config file
+	cfg := config.MustLoad()
 	// 2. Init logger for REST app
 	// 3. Init database connection
 	// 4. Init Server and Handlers
